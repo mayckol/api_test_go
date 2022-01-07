66424264b12e05025457548bb87ddf341edce33e
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2022-01-07 12:19:03 -0300

    adiciona cors

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/mayckol/api_test_go/database"
 	"github.com/mayckol/api_test_go/routes"
 )
@@ -9,6 +10,9 @@ import (
 func main() {
 	database.Connect()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 	routes.Register(app)
 	app.Listen(":3000")
 }
