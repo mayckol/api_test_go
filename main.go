f9068ea244d978c79b592a60bba719ad0ca2d01b
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2022-01-07 10:19:41 -0300

    correcao importacao modulos

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"./routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/mayckol/api_test_go/database"
+	"github.com/mayckol/api_test_go/routes"
 )
 
 func main() {
-	// database.Connect()
+	database.Connect()
 	app := fiber.New()
 	routes.Setup(app)
 	app.Listen(":3000")
