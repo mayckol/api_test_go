40dc23a5f1979b915685fa5b0b66e2c1d9b5e985
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2022-01-07 11:39:17 -0300

    adiciona lib bcrypt para salvar a senha

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,12 +1,25 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mayckol/api_test_go/models"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
+
 	err := c.BodyParser(&data)
 	if err != nil {
 		return err
 	}
-	return c.JSON(data)
+
+	// password precisa ser convertido para um array de bites para GenerateFromPassword
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+	return c.JSON(user)
 }
--- go.mod
+++ go.mod
@@ -17,6 +17,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
-
