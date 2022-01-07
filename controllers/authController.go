0053976392d4115356fc7139f6347dfe60072a7f
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2022-01-07 11:51:57 -0300

    adiciona funcao login

--- controllers/authController.go
+++ controllers/authController.go
@@ -25,3 +25,31 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 	return c.JSON(user)
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	err := c.BodyParser(&data)
+	if err != nil {
+		return err
+	}
+
+	var user models.User
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status((fiber.StatusNotFound))
+		return c.JSON((fiber.Map{
+			"message": "user not found",
+		}))
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	return c.JSON(user)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -7,4 +7,5 @@ import (
 
 func Register(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
 }
