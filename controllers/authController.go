f9627d7bfc3014dc3c4289bf821b8b2e963b072c
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2022-01-07 12:38:07 -0300

    adiciona endpoint user

--- controllers/authController.go
+++ controllers/authController.go
@@ -83,3 +83,20 @@ func Login(c *fiber.Ctx) error {
 		"message": "success!",
 	})
 }
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		c.Status((fiber.StatusUnauthorized))
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+	claims := token.Claims
+	return c.JSON(claims)
+}
