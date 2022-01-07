d72c345419501ea625196a619348387b2acf1bfa
Mayckol Ferreira <mayckol.ferreira@basis.com.br>
2022-01-07 12:10:28 -0300

    adiciona token jwt

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"strconv"
+
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/gofiber/fiber/v2"
 	"github.com/mayckol/api_test_go/database"
 	"github.com/mayckol/api_test_go/models"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const SecretKey = "secret"
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -51,5 +56,18 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	return c.JSON(user)
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		// ExpiresAt: time.Now().Add(time.Minute * 15).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "could not login",
+		})
+	}
+
+	return c.JSON(token)
 }
